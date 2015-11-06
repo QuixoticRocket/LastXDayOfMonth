80f5f57cb6d3ee62adc7e4fc08309b7d8b57d908
Alex <quixoticrocket@gmail.com>
2015-11-06 09:10:47 +0000

    most of the program
    could use some renaming.
    console app doesn't do anything

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using datefiddlystuff;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Nov2015Monday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Monday, 11, 2015);
+
+            Assert.AreEqual(30, answer);
+        }
+
+        [TestMethod]
+        public void Nov2015Tuesday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Tuesday, 11, 2015);
+
+            Assert.AreEqual(24, answer);
+        }
+        [TestMethod]
+        public void Nov2015Wednesday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Wednesday, 11, 2015);
+
+            Assert.AreEqual(25, answer);
+        }
+        [TestMethod]
+        public void Nov2015Thursday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Thursday, 11, 2015);
+
+            Assert.AreEqual(26, answer);
+        }
+        [TestMethod]
+        public void Nov2015Friday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Friday, 11, 2015);
+
+            Assert.AreEqual(27, answer);
+        }
+        [TestMethod]
+        public void Nov2015Saturday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Saturday, 11, 2015);
+
+            Assert.AreEqual(28, answer);
+        }
+        [TestMethod]
+        public void Nov2015Sunday()
+        {
+            var answer = staticFiddler.GetLastDayOfMonth(DayOfWeek.Sunday, 11, 2015);
+
+            Assert.AreEqual(29, answer);
+        }
+    }
+}
---
+++ datefiddlystuff/staticFiddler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datefiddlystuff
+{
+    public static class staticFiddler
+    {
+        public static int GetLastDayOfMonth(DayOfWeek day, int month, int year)
+        {
+            var numberOfDays = DateTime.DaysInMonth(year, month);
+            DateTime workingDate = new DateTime(year, month, numberOfDays);
+            for (int i = 1; i < 7; i++)
+            {
+                if (workingDate.DayOfWeek == day)
+                {
+                    return workingDate.Day;
+                }
+
+                workingDate = workingDate.AddDays(-1);
+            }
+
+            return workingDate.Day;
+        }
+    }
+}
